3d597e8fd64fe7ce267f1738db058f73b1d26d31
vermad843 <vishnuverma7524@gmail.com>
2020-09-12 03:49:22 +0530

    searchbar

--- amazon-clone/README.md
+++ amazon-clone/README.md
@@ -1,2 +1,9 @@
 [x] React-Reouter for multipage non refereshing pages
 
+[x] npm install react-router-dom
+
+[x]img = "http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+
+[x] Material UI -  npm install @material-ui/core
+
+[x]Icons - npm install @material-ui/icons
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './Header.css';
+import {
+    Link
+  } from "react-router-dom";
+  import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
     return (
        <nav className = "header">
-          <img 
-             className = "header_logo" 
-             src = "http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-             alt = ""    
-             />
+          <Link to = "/">
+             <img 
+                className = "header_logo" 
+                src = "http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt = ""    
+                />
+          </Link>
+          <div className = "header_search">
+            <input type = "text" className = "header_searchInput"/>
+             <SearchIcon className = "header_searchIcon"/>
+          </div>
        </nav> 
     );
 }
