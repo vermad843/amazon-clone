b1534295765281e323c30eb5e337f37b0a8c13dd
vermad843 <vishnuverma7524@gmail.com>
2020-09-24 09:15:38 +0530

    one card

--- amazon-clone/README.md
+++ amazon-clone/README.md
@@ -13,3 +13,5 @@
 [x] rfce command for creating functional component
 
 [x] banner image src = "https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Rajeshwari/AugOffers/GW22Aug/DesktopHero_1500x600._CB407658174_.jpg"
+
+[x] nike T-shirt - https://images-na.ssl-images-amazon.com/images/I/91iNo-IecrL._UL1500_.jpg
--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import  './Home.css';
+import Product from './Product';
 
 function Home() {
     return (
@@ -7,6 +8,13 @@ function Home() {
            <img
              className = "home_image" 
              src  = "https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Rajeshwari/AugOffers/GW22Aug/DesktopHero_1500x600._CB407658174_.jpg" alt = ""/>
+             <Product
+               id = '12321341'
+               title = "The lean Startup"
+               price = {11.96}
+               rating = {3}
+               image = "https://images-na.ssl-images-amazon.com/images/I/91iNo-IecrL._UL1500_.jpg"
+             />
         </div>
     )
 }
