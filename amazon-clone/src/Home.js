bd3a1363c11a0a3c64e5875aadf49452782268d1
vermad843 <vishnuverma7524@gmail.com>
2020-09-24 09:32:16 +0530

    2,3,1

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -8,13 +8,54 @@ function Home() {
            <img
              className = "home_image" 
              src  = "https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Rajeshwari/AugOffers/GW22Aug/DesktopHero_1500x600._CB407658174_.jpg" alt = ""/>
-             <Product
-               id = '12321341'
-               title = "The lean Startup"
-               price = {11.96}
-               rating = {3}
-               image = "https://images-na.ssl-images-amazon.com/images/I/91iNo-IecrL._UL1500_.jpg"
-             />
+            <div className = "home__row">
+               <Product
+                  id = '12321341'
+                  title = "The lean Startup"
+                  price = {11.96}
+                  rating = {3}
+                  image = "https://images-na.ssl-images-amazon.com/images/I/91iNo-IecrL._UL1500_.jpg"
+                />
+                 <Product
+                  id = '12321341'
+                  title = "The lean Startup"
+                  price = {11.96}
+                  rating = {3}
+                  image = "https://images-na.ssl-images-amazon.com/images/I/91iNo-IecrL._UL1500_.jpg"
+                />
+            </div>
+            <div className = "home__row">
+               <Product
+                  id = '12321341'
+                  title = "The lean Startup"
+                  price = {11.96}
+                  rating = {3}
+                  image = "https://images-na.ssl-images-amazon.com/images/I/91iNo-IecrL._UL1500_.jpg"
+                />
+                 <Product
+                  id = '12321341'
+                  title = "The lean Startup"
+                  price = {11.96}
+                  rating = {3}
+                  image = "https://images-na.ssl-images-amazon.com/images/I/91iNo-IecrL._UL1500_.jpg"
+                />
+                  <Product
+                  id = '12321341'
+                  title = "The lean Startup"
+                  price = {11.96}
+                  rating = {3}
+                  image = "https://images-na.ssl-images-amazon.com/images/I/91iNo-IecrL._UL1500_.jpg"
+                />
+            </div>
+            <div className = "home_row">
+                 <Product
+                  id = '12321341'
+                  title = "The lean Startup"
+                  price = {11.96}
+                  rating = {3}
+                  image = "https://images-na.ssl-images-amazon.com/images/I/91iNo-IecrL._UL1500_.jpg"
+                />
+            </div>
         </div>
     )
 }
