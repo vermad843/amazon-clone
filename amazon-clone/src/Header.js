1e2c8b7907d755b4c619fd5d953cf17914374cc8
vermad843 <vishnuverma7524@gmail.com>
2020-09-25 09:12:29 +0530

    correct one

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -5,8 +5,11 @@ import {
   } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import {useStateValue} from './StateProvider';
 
 function Header() {
+     const [{basket}] = useStateValue();
+
     return (
        <nav className = "header">
           <Link to = "/">
@@ -42,7 +45,7 @@ function Header() {
               <Link to = "/checkout" className = "header_link">
                    <div className = "header_optionBasket">
                       <ShoppingBasketIcon/>
-                      <span className = "header_optionLineTwo header_basketCount">0</span>
+                      <span className = "header_optionLineTwo header_basketCount">{basket.length}</span>
                    </div>
               </Link>
           </div>
--- amazon-clone/src/index.js
+++ amazon-clone/src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {StateProvider} from './StateProvider';
-import reducer, { initialState } from './reducer';
+import reducer,{ initialState } from './reducer';
+
 
 ReactDOM.render(
   <React.StrictMode>
